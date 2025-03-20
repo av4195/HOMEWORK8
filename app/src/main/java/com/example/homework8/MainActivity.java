5f4c4503389038f9aa1eca92d72d2680817a4505
av4195 <av4195@bs.amalnet.k12.il>
2025-03-20 16:41:00 +0200

    עיצוב

--- app/src/main/java/com/example/homework8/MainActivity.java
+++ app/src/main/java/com/example/homework8/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.homework8;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1;
+    TextView tV1up;
+    int num;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn1 = findViewById(R.id.btn1);
+        tV1up = findViewById(R.id.tV1up);
+
+    }
+
+    public void stage01(View view) {
+        tV1up.setText("הטקסט החדש שלך");
     }
 }
